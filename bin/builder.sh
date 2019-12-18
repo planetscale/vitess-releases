2f2165283dbf8bb1348dfece74c1f0797c4e9fee
Gary Edgar <gary@planetscale.com>
2019-12-18 13:53:59 -0800

    builder.sh tweaks

--- bin/builder.sh
+++ bin/builder.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # This script builds and packages a Vitess release suitable for creating a new
 # release on https://github.com/vitessio/vitess/releases.
@@ -12,8 +12,6 @@ sudo apt-get -y install make automake \
     build-essential ruby-dev rubygems rpm
 
 sudo gem install --no-ri --no-rdoc fpm
-
-DIR=$PWD/$(dirname "$0")
 
 INSTALL_GO=0
 if [ -f /usr/local/go/bin/go ]; then
@@ -43,59 +41,8 @@ if [ ! -d src/vitess.io/vitess ]; then
 fi
 
 cd src/vitess.io/vitess
+#git checkout remotes/planetscale/$1
+#git pull planetscale $1
 git checkout master
-git pull
-
-BUILD_TESTS=0 ./bootstrap.sh
-make build
-
-SHORT_REV="$(git rev-parse --short HEAD)"
-RELEASE_ID="vitess-4.0.0-${SHORT_REV}"
-
-mkdir -p ~/releases
-RELEASE_DIR=${HOME}/releases/${RELEASE_ID}
-
-for d in bin lib dist config pkg/linux_amd64 vthook; do
-    mkdir -p "${RELEASE_DIR}/$d"
-    echo cp -Lrpf "$HOME/go/$d/." "${RELEASE_DIR}/$d"
-    cp -Lrpf "$HOME/go/$d/." "${RELEASE_DIR}/$d"
-done
-
-for d in chromedriver grpc MYSQL_FLAVOR py-mock-1.0.1 selenium vt-protoc-3.6.1; do
-    rm -rf "${RELEASE_DIR}/dist/$d"
-done
-
-mkdir -p "${RELEASE_DIR}/web"
-cp -rpf "$HOME/go/src/vitess.io/vitess/web/." "${RELEASE_DIR}/web"
-
-mkdir -p "${RELEASE_DIR}/examples"
-cp -rpf "$HOME/go/src/vitess.io/vitess/examples/." "${RELEASE_DIR}/examples"
-
-cp "${DIR}/release_README.md" "${RELEASE_DIR}/README.md"
-
-cd "${RELEASE_DIR}/.."
-TAR_FILE="${RELEASE_ID}.tar.gz"
-tar -czf "${TAR_FILE}" "${RELEASE_ID}"
-
-"${DIR}"/make_package.sh \
-    -C "${RELEASE_DIR}" \
-    --package "$(dirname "${RELEASE_DIR}")" \
-    --iteration "${SHORT_REV}" \
-    -t deb --deb-no-default-config-files
-DEB_FILE="vitess_4.0.0-${SHORT_REV}_amd64.deb"
-
-"${DIR}"/make_package.sh \
-    -C "${RELEASE_DIR}" \
-    --package "$(dirname "${RELEASE_DIR}")" \
-    --iteration "${SHORT_REV}" \
-    -t rpm
-RPM_FILE="vitess-4.0.0-${SHORT_REV}.x86_64.rpm"
-
-echo ""
-echo "Packages created as of $(date +"%m-%d-%y") at $(date +"%r %Z")"
-echo ""
-echo "Package | SHA256"
-echo "------------ | -------------"
-echo "${TAR_FILE} | $(sha256sum ~/releases/"${TAR_FILE}" | awk '{print $1}')"
-echo "${DEB_FILE} | $(sha256sum ~/releases/"${DEB_FILE}" | awk '{print $1}')"
-echo "${RPM_FILE} | $(sha256sum ~/releases/"${RPM_FILE}" | awk '{print $1}')"
+git pull 
+./tools/make-release-packages.sh
